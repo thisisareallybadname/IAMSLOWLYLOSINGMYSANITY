318e19031380a0b75969e25634acb47874317f86
thisisareallybadname <unparallelgrip@gmail.com>
2024-05-08 09:43:57 -0700

    pp
    pp

--- Assets/Scripts/GameMechanics/DropBomb.cs
+++ Assets/Scripts/GameMechanics/DropBomb.cs
@@ -3,56 +3,74 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class LandmineSetter : MonoBehaviour {
+public class LandmineSetter : MonoBehaviour
+{
 
     public WaveManager manager;
     private float bombCounter;
     public float bombLimit;
 
     private bool canDropBombs;
+    private bool spawningLandmineIndicators;
 
     public float floorX;
     public float floorZ;
 
     public GameObject landmine;
-    
+    public GameObject landmineIndicator;
+
+    private HashSet<GameObject> landmineIndicators = new HashSet<GameObject>();
     private HashSet<GameObject> landmines = new HashSet<GameObject>();
 
     // Start is called before the first frame update
-    
-    void Start() {
+
+    void Start()
+    {
 
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
 
-        canDropBombs = manager.intermission;
+        spawningLandmineIndicators = manager.intermission;
+        canDropBombs = manager.spawningEnemies;
 
         bombLimit = manager.wave * 5;
 
-        if (canDropBombs) {
-            if (bombCounter < bombLimit) {
+        if (spawningLandmineIndicators)
+        {
+            if (bombCounter < bombLimit)
+            {
                 transform.position = new Vector3(Random.Range(-floorX / 2, floorX / 2), 3, Random.Range(-floorZ / 2, floorZ / 2));
-                GameObject newLandmine = Instantiate(landmine, transform.position, Quaternion.identity);
-                newLandmine.GetComponent<ProjectileBehavior>().dangerous = true;
-
-                landmines.Add(newLandmine);
+                GameObject newLandmineIndicator = Instantiate(landmineIndicator, transform.position, Quaternion.identity);
+                landmineIndicators.Add(newLandmineIndicator);
 
                 bombCounter++;
 
             }
 
         } else {
-            bombCounter = 0;
 
-                foreach (GameObject landmine in landmines) {
-                    Destroy(landmine);
-
+            if (canDropBombs) {
+                foreach (GameObject indicator in landmineIndicators) {
+                    GameObject newLandmine = Instantiate(landmine, indicator.transform.position, Quaternion.identity);
+                    newLandmine.GetComponent<ProjectileBehavior>().dangerous = true;
+                    landmines.Add(newLandmine);
+                    //Destroy(indicator);
+                    //Debug.Log(landmineIndicators.Count - landmines.Count);
                 }
 
-            
-            
+            } else {
+                bombCounter = 0;
+
+                if (manager.EnemiesLeft() == 0) {
+                    foreach (GameObject landmine in landmines) {
+                        Destroy(landmine);
+
+                    }
+                }
+            }
         }
     }
 

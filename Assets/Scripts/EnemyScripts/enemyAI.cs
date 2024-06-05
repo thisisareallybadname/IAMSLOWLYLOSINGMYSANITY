6e9e94d5a65b87da425f1ea0edf6ea6d8e6b3011
thisisareallybadname <161801289+thisisareallybadname@users.noreply.github.com>
2024-06-05 06:53:42 -0700

    updated EnemyAI comments

--- Assets/Scripts/EnemyScripts/enemyAI.cs
+++ Assets/Scripts/EnemyScripts/enemyAI.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// script that controls the enemy's AI
 public class enemyAI : MonoBehaviour {
 
     public GameObject player;
-    public int walkspeed;
+    public int walkspeed; 
     public Rigidbody rb;
 
     public bool canFireProjectiles;
-    private bool chasePlayer;
 
     public GameObject projectile;
 
@@ -30,39 +30,44 @@ public class enemyAI : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate() {
-        gameObject.transform.LookAt(player.transform);
+        gameObject.transform.LookAt(player.transform); // look at the player
 
+            // move rigidbody to position in front of enemy, so it will go towards player
             rb.AddForce(transform.forward * walkspeed * 200 * Time.deltaTime, ForceMode.Force);
-            rb.drag = 2.5f;
+            rb.drag = 1.25f;
 
+            // a little bit of acceleration because why not
             rb.velocity += walkspeed * (rb.transform.forward.normalized / 7.5f);
-            
-            if (fireCooldown >= projectileFirerate && canFireProjectiles) {
-                FireProjectile();
-                fireCooldown = 0;
 
-            } else {
-                fireCooldown += Time.fixedDeltaTime;
-
+            // if the enemy is a ranged enemy, fire a projectile 
+            if (canFireProjectiles) {
+                if (fireCooldown >= projectileFirerate) {
+                    FireProjectile();
+                    fireCooldown = 0;
+    
+                } else {
+                    fireCooldown += Time.fixedDeltaTime;
+    
+                }
             }
-
-        
     }
 
+    // make a new enemyprojectile object
     private void FireProjectile() {
+
+        // nake fireball object
         GameObject newFireball = Instantiate(projectile, transform.position + transform.forward, transform.rotation);
         ProjectileBehavior fireballProperties = newFireball.GetComponent<ProjectileBehavior>();
+        
+        // by default the projectile's behavior is disabled so it won't accidentally blow up
         fireballProperties.enabled = true;
         fireballProperties.canExplode = true;
         fireballProperties.walkspeed = projectileSpeed;
     }
 
+    // set the movespeed of the enemy
     public void setMovementSpeed(float newWalkspeed) {
 
         walkspeed = (int)newWalkspeed;
-    }
-
-    public void MakeHostile(bool chase) {
-        chasePlayer = chase;
     }
 }

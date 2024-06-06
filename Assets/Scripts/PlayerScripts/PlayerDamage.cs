e0e78d908aa567c15f0866947cc5265b54794b23
thisisareallybadname <161801289+thisisareallybadname@users.noreply.github.com>
2024-06-06 11:06:34 -0700

    playerdamage- added comments
    adadadadad

--- Assets/Scripts/PlayerScripts/PlayerDamage.cs
+++ Assets/Scripts/PlayerScripts/PlayerDamage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
 
+// manages damage intake player gets
 public class PlayerDamage : MonoBehaviour {
     private CharacterController controller;
         
@@ -13,14 +14,19 @@ public class PlayerDamage : MonoBehaviour {
 
     public float health;
 
+    // doctors' recommended damage intake is 1 dmg / 1 sec
+    
+    // immunity timers 
     private float immunityTimer;
     public float immunityLength;
     private bool canTakeDamage;
 
+    // shove player camera every time player takes damage thru playerCamera
     public PlayerCamera hurtEffect;
     public TimeManager timeManager;
     public UIManager uiManager;
 
+    // make sure that timaManager doesn't get spammed w/ stopGame()
     private bool deathDebounce;
 
     public float maxHealth;
@@ -39,48 +45,53 @@ public class PlayerDamage : MonoBehaviour {
             immunityTimer += Time.deltaTime;
 
         }
-
-        if (immunityTimer >= immunityLength)
-        {
+        if (immunityTimer >= immunityLength) {
             canTakeDamage = true;
             immunityTimer = 0;
         }
-        
+
+        // make cursor work normally again when health = 0``
         if (health <= 0) {
             health = 0;
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
 
+            // end the game when player dead
             if (deathDebounce) {
                 deathDebounce = false;
                 timeManager.endGame();
 
             }
         } else {
-            deathDebounce = true;
 
+            // reset debounce var when player isn't dead
+            if (!deathDebounce) {
+                deathDebounce = true;
+            
+            }
         }
 
     }
 
-
+    // if player is touching an enemy and if they have no iframes, make them take damage
     private void OnTriggerStay(Collider collision) {
         if (collision.gameObject.tag.Equals("Enemy") && collision.gameObject.GetComponent<EnemyHealth>().getHealth() > 0) {
-            takeDamage(collision.gameObject.GetComponent<Enemy>().getDamage());
-
+            takeDamage(1);
+            
         }
-
     }
 
+    // get player's health, used in player UI script
     public float getHealth() {
         return health;
     }
 
+    // set max health, with three modes, add, mutl, and set
     public void setMaxHealth(float newHealth, string mode) {
         if (mode.Equals("add")) {
             maxHealth += newHealth;
 
-        } else if (mode.Equals("mult")) {
+        } else if (mode.Contains("mult")) { //contains because im very inconsistent in typing thingy
             maxHealth *= newHealth;
 
         } else {
@@ -88,21 +99,27 @@ public class PlayerDamage : MonoBehaviour {
 
         }
 
+        health = maxHealth;
     }
 
+    // get max health, used in player UI script
     public float getMaxHealth() {
         return maxHealth;
 
     }
 
+    // reset stats (what did you expect?????)
     public void resetStats() {
         maxHealth = 5;
 
     }
 
+    // take damage if theres no iframes
     public void takeDamage(float damage) {
         if (canTakeDamage) {
             canTakeDamage = false;
+
+            // camera hurt effect
             hurtEffect.applyCameraForce(new Vector3(0, 0, -22.5f), new Vector3(0, 0.75f, -0.5f));
             health -= damage;
 
@@ -110,17 +127,9 @@ public class PlayerDamage : MonoBehaviour {
 
     }
 
+    // set health
     public void setHealth(float newHealth) {
         health = newHealth;
 
     }
-
-    private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.tag.Equals("DEATH")) {
-            takeDamage(999999999999999999);
-
-        }
-        
-    }
-
 }
